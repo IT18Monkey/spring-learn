1586dfd0e694b9c8dc092aeeeab68e5c56f6d1c9
IT18Monkey <15151967329@163.com>
2017-05-09 18:19:46 +0800

    配置

---
+++ src/main/java/com/whh/business/service/CDPlayer.java
@@ -0,0 +1,8 @@
+package com.whh.business.service;
+
+/**
+ * @Author 王豪豪
+ * @Date 2017/5/9 18:19
+ */
+public class CDPlayer {
+}
---
+++ src/main/java/com/whh/config/SpringConfig.java
@@ -0,0 +1,12 @@
+package com.whh.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author 王豪豪
+ * @Date 2017/5/9 17:41
+ */
+@Configuration
+public class SpringConfig {
+
+}
